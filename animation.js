d7a484562a547b6181390a70e077e3b27360655e
Nishant Singh <singhnishant414243@gmail.com>
2025-05-12 23:11:09 +0530

    Update menubar and add animation in menuebar items

--- animation.js
+++ animation.js
@@ -81,39 +81,6 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", changeActiveNav);
 });
 
-// ========== MOUSE MOVE IFFECT
-(function () {
-    const links = document.querySelectorAll('.nav-bar li');
-    const cursor = document.querySelector('.cursor');
-
-    const animateIt = function (e) {
-        const span = this.querySelector('span');
-        const { offsetX: x, offsetY: y } = e,
-            { offsetWidth: width, offsetHeight: height } = this,
-            move = 25,
-            xMove = (x / width) * (move * 2) - move,
-            yMove = (y / height) * (move * 2) - move;
-
-        span.style.transform = `translate(${xMove}px, ${yMove}px)`;
-        if (e.type === 'mouseleave') span.style.transform = '';
-    };
-
-    const editCursor = (e) => {
-        const { clientX: x, clientY: y } = e;
-        cursor.style.left = `${x}px`;
-        cursor.style.top = `${y}px`;
-    };
-
-    links.forEach(link => {
-        link.addEventListener('mousemove', animateIt);
-        link.addEventListener('mouseleave', animateIt);
-    });
-
-    window.addEventListener('mousemove', editCursor);
-})();
-
-
-
 // ==================================== HOME SECTION ===============================================
 document.addEventListener("DOMContentLoaded", () => {
     const zoomSection = document.querySelector(".firstsection");
